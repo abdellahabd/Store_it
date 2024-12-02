0d465471ddc2ccef22fd4b0d26692d3104384987
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-12-02 14:10:01 +0100

    add assets and icons

---
+++ app/(auth)/sing-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingIn() {
+  return (
+    <div>Sing In</div>
+  )
+}
+
+export default SingIn
---
+++ app/(auth)/sing-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingUp() {
+  return (
+    <div>Sing Up</div>
+  )
+}
+
+export default SingUp
